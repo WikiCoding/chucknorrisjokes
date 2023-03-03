96b77df41dff11014da4cc18c24823ca59e9c793
Tiago Castro <121066161+WikiCoding@users.noreply.github.com>
2023-03-03 16:22:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const fetchData = async () => {
   const fetchData = await fetch('https://api.chucknorris.io/jokes/random', {
     method: 'GET'
   })
-  const joke = await fetchData.json();
+  try {
+    const joke = await fetchData.json();
 
-  myDiv.innerHTML = joke.value
+    myDiv.innerHTML = joke.value
+  } catch (e) {
+    myDiv.innerHTML = "Sorry couldn't get your joke, try refreshing the page"
+  }
 }
 
 fetchData();
